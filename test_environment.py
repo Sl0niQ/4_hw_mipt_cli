e9abbb13519f88feeafb16dd94b57fead1c9142e
Sl0niQ <31eriflleh@gmail.com>
2025-08-07 13:51:18 +0700

    test environment describtion added to readme; test_environment script completed

--- test_environment.py
+++ test_environment.py
@@ -1,19 +1,58 @@
 import os
 
-current_path = os.getcwd() ###current_path?
-test_path = os.path.join(current_path, 'test1')
-if os.path.exists(test_path):
-	print(f'error: before running tests, you should delete the directory {test_path}')
-else:
-	os.makedirs(os.path.join(test_path, 'test2', 'test3'))  
+def create_environment() -> int:
+    """
+        creates folders and files to test the app 'manager'
+        <current folder>\\test\\test.txt
+        <current folder>\\test\\test1\\test.txt
+        <current folder>\\test\\test1\\test2\\test.txt
+    """    
+    test_path = os.path.join(os.getcwd(), 'test')
+    if os.path.exists(test_path):
+        print(f'error: before running tests, you should clear the test environment by running the remove_environment() function')
+        return -1
+    else:
+    	os.makedirs(os.path.join(test_path, 'test1', 'test2'))  
 
-filename = os.path.join(test_path, 'test.txt')
-if not os.path.isfile(filename):
-    with open(filename, 'w') as f:
-        f.write('')
-else:
-    print(f'error: file <{filename}> already exists!')
+    filename = os.path.join(test_path, 'test.txt')
+    if not os.path.isfile(filename):
+        with open(filename, 'w') as f:
+            f.write('Q' * 100)
+    else:
+        print(f'error: file <{filename}> already exists!')
 
-### Доделать создание тестового окружения - создать еще один файл (ненулевой)
-### сделать очистку тестового окружения
-### обернуть в функции
+    filename = os.path.join(test_path, 'test1', 'test.txt')
+    if not os.path.isfile(filename):
+        with open(filename, 'w') as f:
+            f.write('Q' * 10000)
+    else:
+        print(f'error: file <{filename}> already exists!')
+
+    filename = os.path.join(test_path, 'test1', 'test2',  'test.txt')
+    if not os.path.isfile(filename):
+        with open(filename, 'w') as f:
+            f.write('Q' * 1000000)
+    else:
+        print(f'error: file <{filename}> already exists!')        
+    return 0
+
+def remove_environment(test_path = '') -> int:
+    """
+        removes test environment from current folder
+    """    
+    test_path = test_path if test_path else os.path.join(os.getcwd(), 'test')
+    if os.path.exists(test_path):
+        for item in os.listdir(test_path):
+            local_path = os.path.join(test_path, item)
+            if os.path.isdir(local_path):
+                remove_environment(local_path)
+            else:
+                os.remove(local_path)
+        os.rmdir(test_path)
+        return 0
+    else:
+        print(f'test environment has already been removed')
+        return -1 
+
+#create_environment()
+remove_environment()
